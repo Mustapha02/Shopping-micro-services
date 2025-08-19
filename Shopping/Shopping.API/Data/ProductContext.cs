01d86fd8d05e0bb97e32e73966b9c39450bfd469
mustapha <mustapha@tanitweb.com>
2025-08-19 22:26:29 +0100

    test api

--- Shopping/Shopping.API/Data/ProductContext.cs
+++ Shopping/Shopping.API/Data/ProductContext.cs
@@ -80,7 +80,7 @@ namespace Shopping.API.Data
                     Description = "This phone is the company's biggest change to its flagship smartphone in years. It includes a borderless.",
                     ImageFile = "product-6.png",
                     Price = 240.00M,
-                    Category = "Home Kitchen"
+                    Category = "H"
                 }
             };
         }
